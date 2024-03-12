bcc2ed4c55acd3b8aaafbcc2508f9fd57dac4923
nembutaldev <ce.rodriguez240391@gmail.com>
2024-03-11 23:44:59 -0600

    fix: errores de build

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,18 @@ import ProductosCatalogo from "./pages/ProductosCatalogo/ProductosCatalogo";
 
 const App = () => {
   return (
-    <>
+    <div>
       <div className="gradient"></div>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <ProductosCatalogo />
       </ThemeProvider>
-    </>
+    </div>
   );
 };
 const rootElement = document.getElementById("app");
 if (!rootElement) throw new Error("Failed to find the root element");
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement);
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(<App />);
